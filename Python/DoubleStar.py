5d5b2388ce4c044ca83d412edd853a71c21391d3
jventura10 <jventura1097@gmail.com>
2017-11-14 03:58:55 +0000

    Create DoubleStar.py

---
+++ Python/DoubleStar.py
@@ -0,0 +1,16 @@
+import turtle
+
+wn =turtle.Screen()
+jave=turtle.Turtle()
+leny=turtle.Turtle()
+
+for i in range(50):
+    jave.forward(300)
+    jave.right(500)
+
+
+for i in range(50):
+    leny.back(300)
+    leny.left(500)
+
+turtle.done()
