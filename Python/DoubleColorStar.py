555df0e6b6cc5692d2631af90f4ccdb92fb5188b
jventura10 <jventura1097@gmail.com>
2017-11-14 03:57:19 +0000

    Create DoubleColorStar.py

---
+++ Python/DoubleColorStar.py
@@ -0,0 +1,18 @@
+import turtle
+
+wn=turtle.Screen()
+
+jave=turtle.Turtle()
+
+jave.pencolor("green")
+for i in range(50):
+    jave.forward(50)
+    jave.left(123)
+    
+jave.pencolor("red")
+for i in range(50):
+    jave.forward(100)
+    jave.left(123)
+
+turtle.done()
+    
